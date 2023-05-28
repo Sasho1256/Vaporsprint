44f19d9021d2b989b39867e6d1bf3f3515384184
dercodegott <wtseller0@gmail.com>
2023-05-28 16:54:27 +0200

    added moving obstacles/enemies

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float movementRange; // movement to the left on the x-axis 
+    public float speed;
+    private Vector3 startpos;
+    private bool goRight; //is true if the object is headed to the right side
+
+    void Start()
+    {
+        startpos = transform.position;
+    }
+
+    void Update()
+    {
+        if (IsInCameraView())
+            Move();
+    }
+
+    private void Move()
+    {
+        if (!goRight)
+        {
+            if (transform.position.x > startpos.x - movementRange)
+            {
+                transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+            } else
+            {
+                goRight = true;
+            }
+        }
+        if (goRight)
+        {
+            if (transform.position.x < startpos.x)
+            {
+                transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                goRight = false;
+            }
+        }
+    }
+
+    private bool IsInCameraView()
+    {
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+        if (screenPoint.z >= 0 && screenPoint.x >= 0 && screenPoint.x <= 3 && screenPoint.y >= 0 && screenPoint.y <= 3)
+        {
+            // The object is in the range of the camera + an overhead
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
