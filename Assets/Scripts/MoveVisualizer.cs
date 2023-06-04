8661e170f2e7e16be2675a152c8beb00ac4fa399
Sasho1256 <atmilachkov@gmail.com>
2023-06-04 14:26:45 +0200

    made the backgrounds move up and down two times slower than the character by adding a public backgroundId property and different update logic respectfully

--- Assets/Scripts/MoveVisualizer.cs
+++ Assets/Scripts/MoveVisualizer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveVisualizer : MonoBehaviour
 {
     public Transform player;
+    public int backgroundID;
 
     //public float y;
     //public float z;
@@ -12,6 +13,26 @@ public class MoveVisualizer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        if(player.position.y > -3){
+            if (backgroundID == 0)
+            {
+                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y+5.86f*2)/2, transform.position.z);
+            }
+            if (backgroundID == 1)
+            {
+                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y+5.37f*2)/2, transform.position.z);
+            }
+            if (backgroundID == 2)
+            {
+                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y-5.52f*2)/2, transform.position.z);
+            }
+            if (backgroundID == 3)
+            {
+                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y-16.63f*2)/2, transform.position.z);
+            }
+        }
+        else{
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
     }
 }
