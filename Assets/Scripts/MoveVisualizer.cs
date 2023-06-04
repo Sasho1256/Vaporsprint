e24c55cffdfdc4cc5910c6f3853d45ce3c7e657d
Sasho1256 <atmilachkov@gmail.com>
2023-06-04 17:05:47 +0200

    documented what the different backgroundIDs refer to

--- Assets/Scripts/MoveVisualizer.cs
+++ Assets/Scripts/MoveVisualizer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveVisualizer : MonoBehaviour
 {
     public Transform player;
-    public int backgroundID;
+    public int backgroundID; // 0 = Background_City_Top; 1 = RetroSun; 2 = Background_Transition; 3 = Background_Under
 
     //public float y;
     //public float z;
@@ -16,19 +16,19 @@ public class MoveVisualizer : MonoBehaviour
         if(player.position.y > -3){
             if (backgroundID == 0)
             {
-                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y+5.86f*2)/2, transform.position.z);
+                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y+5.86f* 1.5f) /1.5f, transform.position.z);
             }
             if (backgroundID == 1)
             {
-                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y+5.37f*2)/2, transform.position.z);
+                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y+5.37f* 1.5f) /1.5f, transform.position.z);
             }
             if (backgroundID == 2)
             {
-                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y-5.52f*2)/2, transform.position.z);
+                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y-5.52f* 1.5f) /1.5f, transform.position.z);
             }
             if (backgroundID == 3)
             {
-                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y-16.63f*2)/2, transform.position.z);
+                transform.position = new Vector3(player.transform.position.x, (player.transform.position.y-16.63f* 1.5f) /1.5f, transform.position.z);
             }
         }
         else{
