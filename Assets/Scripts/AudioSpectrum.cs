0484cb358c43ceece92d482c1bf73f058284e8eb
Skali14 <simon.kadlec@outlook.de>
2023-06-08 17:00:23 +0200

    added comments to AudioSpectrum.cs

--- Assets/Scripts/AudioSpectrum.cs
+++ Assets/Scripts/AudioSpectrum.cs
@@ -16,11 +16,11 @@ public class AudioSpectrum : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        AudioListener.GetSpectrumData(m_audioSpectrum, 0, FFTWindow.Hamming);
+        AudioListener.GetSpectrumData(m_audioSpectrum, 0, FFTWindow.Hamming); //gets the audiospectrum data from all currently playing sounds
 
         if(m_audioSpectrum != null && m_audioSpectrum.Length > 0)
         {
-            spectrumValue = m_audioSpectrum[0] * 200;
+            spectrumValue = m_audioSpectrum[0] * 200; //scales it up, since it would be too small to recognize otherwise
         }
     }
 
