01dba170a4a050f756a82cdbf47c96f9acc737a8
KiraGreifeneder <51335746+KiraGreifeneder@users.noreply.github.com>
2023-06-08 16:26:11 +0200

    added comments in EndLevel

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -7,6 +7,9 @@ public class EndLevel : MonoBehaviour
 {
     public UIManager uiManager;
 
+    //this script is used on the portal GameObject
+    //therefore the function below checks for collision with the player,
+    //i.e. player enters portal/reaches end of level, and then loads the "level complete" screen
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
