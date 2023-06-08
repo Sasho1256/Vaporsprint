c9882797a7a82a60109ec3d3a45dff259876aa47
Skali14 <simon.kadlec@outlook.de>
2023-06-08 16:59:49 +0200

    added comments to AudioSyncer.cs

--- Assets/Scripts/AudioSyncer.cs
+++ Assets/Scripts/AudioSyncer.cs
@@ -20,6 +20,7 @@ public class AudioSyncer : MonoBehaviour
         m_previousAudioValue = m_audioValue;
         m_audioValue = AudioSpectrum.spectrumValue;
 
+        //if the change from one beat to the next one is above a certain bias it triggers OnBeat()
         if(m_previousAudioValue > bias && m_audioValue <= bias)
         {
             if(m_timer > timeStep)
@@ -36,10 +37,10 @@ public class AudioSyncer : MonoBehaviour
             }
         }
 
-        //m_timer += Time.deltaTime;
         m_timer += (float)AudioSettings.dspTime;
     }
 
+    //sets m_isBeat to true, so that classes inheriting from this one can use this to determine a beat
     public virtual void OnBeat()
     {
         m_timer = 0;
