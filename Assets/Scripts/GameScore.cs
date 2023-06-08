14f18301da121b0fadef9d1647b0754353978281
Michi <89996559+wtseller@users.noreply.github.com>
2023-06-08 16:55:20 +0200

    added comments to GameScore.cs

--- Assets/Scripts/GameScore.cs
+++ Assets/Scripts/GameScore.cs
@@ -6,15 +6,15 @@ public class GameScore : MonoBehaviour
 {
     public static float gameScore;
 
-    private void Update()
+    private void Update() //calculates the progress of the player within a level in %
     {
         if(GameOver.gameOver)
         {
             float tmp = 0;
-            float distanceLeftToPlayer = Mathf.Abs(GameObject.Find("LeftMapBound").transform.position.x) + GameObject.Find("Player").transform.position.x;
-            float totalLevelDistance = GameObject.Find("RightMapBound").transform.position.x - GameObject.Find("LeftMapBound").transform.position.x;
+            float distanceLeftToPlayer = Mathf.Abs(GameObject.Find("LeftMapBound").transform.position.x) + GameObject.Find("Player").transform.position.x; //already covered distance by the player
+            float totalLevelDistance = GameObject.Find("RightMapBound").transform.position.x - GameObject.Find("LeftMapBound").transform.position.x; //total distance on the x-axis of the level
             tmp = (distanceLeftToPlayer / totalLevelDistance) * 100;
-            if(tmp > 100)
+            if(tmp > 100) //handles the case of the player jumping beyond the end portal
             {
                 gameScore = 99;
             } else if(tmp < 0)
